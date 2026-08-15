d4596458ac971877d22ad17139f258a19e212fe6
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-15 15:33:33 +0200

    feat(api): uploading thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +30,59 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20 // 10 MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing form data", err)
+		return
+	}
+	file, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error retrieving thumbnail file", err)
+		return
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := r.Header.Get("Content-Type")
+	reader := io.LimitReader(file, maxMemory)
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error reading thumbnail file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error retrieving video metadata", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not authorized to upload a thumbnail for this video", nil)
+		return
+	}
+
+	videoThumbnails[videoID] = thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+
+	url := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoID)
+	video.ThumbnailURL = &url
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video metadata", err)
+		return
+	}
+	fmt.Printf("Updated metadata: %v\n", video)
+	respondWithJSON(w, http.StatusOK, database.Video{
+		ID:           video.ID,
+		CreatedAt:    video.CreatedAt,
+		UpdatedAt:    video.UpdatedAt,
+		ThumbnailURL: video.ThumbnailURL,
+		VideoURL:     video.VideoURL,
+	})
 }
