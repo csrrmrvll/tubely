508622bc964f7fd7cd805eac85c07fc07cc1e539
César Romero Avello <cesar.romero.avello@ing.com>
2026-08-18 09:06:04 +0200

    feat(app): improved using solution files from bootdotdev

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"io"
 	"mime"
 	"net/http"
@@ -52,10 +50,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	key := make([]byte, 32)
-	rand.Read(key)
-	fileName := base64.RawURLEncoding.EncodeToString(key)
-	assetPath := getAssetPath(fileName, mediaType)
+	assetPath := getAssetPath(mediaType)
 	assetDiskPath := cfg.getAssetDiskPath(assetPath)
 
 	dst, err := os.Create(assetDiskPath)
